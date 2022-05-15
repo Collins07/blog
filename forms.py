313df5d2cf2ee7674f3f282e5002a9f16cd73646
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-15 09:23:46 +0300

    registration form

---
+++ forms.py
@@ -0,0 +1,13 @@
+from xml.dom import ValidationErr
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, DateTimeField
+from wtforms.validators import DataRequired, EqualTo, Length,Email
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=10)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    repeat_password = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
+
+    submit = SubmitField('Sign-Up')
