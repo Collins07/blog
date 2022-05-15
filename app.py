e3f57498e38192a534022a712a2a711ba046d548
Collins07 <collins.nyakoe@student.moringaschool.com>
2022-05-15 14:43:48 +0300

    sqlalchemy instance

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+
 posts = [
     {
         'author':'Collins Nyakoe',
